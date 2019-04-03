4ab7f0194db9ce19da9f6214684f995c40b4c0d5
Christian Suloway <christian.suloway@nih.gov>
2019-04-03 10:16:29 -0400

    added model and rest API

--- server/models/colormap.py
+++ server/models/colormap.py
@@ -1,9 +1,103 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import datetime
+
+from bson.binary import Binary
+
+from girder.constants import AccessType
 from girder.models.model_base import AccessControlledModel
+
+
+# for PIL.ImagePalette
+def colormap_to_bytes(colormap, default=None):
+    palette = []
+    for i in range(3):
+        for j in range(256):
+            try:
+                value = int(colormap[j][i])
+            except KeyError:
+                try:
+                    value = int(colormap[str(j)][i])
+                except KeyError:
+                    if default is None:
+                        value = j
+                    else:
+                        value = default[i]
+            palette.append(value)
+    return bytes(bytearray(palette))
 
 
 class Colormap(AccessControlledModel):
     def initialize(self):
         self.name = 'colormap'
+        self.ensureIndices([
+            'created',
+            'creatorId',
+        ])
+        self.ensureTextIndex({
+            'name': 10,
+        })
+
+        self.exposeFields(AccessType.READ, (
+            '_id',
+            'name',
+            'created',
+            'creatorId',
+            'colormap',
+            'labels',
+            'created',
+            'updated',
+            'updatedId',
+            'public',
+            'publicFlags',
+            'groups',
+        ))
+
+    def createColormap(self, creator, colormap, name=None, labels=None,
+                       public=None):
+        now = datetime.datetime.utcnow()
+        doc = {
+            'creatorId': creator['_id'],
+            'created': now,
+            'updatedId': creator['_id'],
+            'updated': now,
+            'colormap': colormap,
+        }
+        if name is not None:
+            doc['name'] = name
+        if labels is not None:
+            doc['labels'] = labels
+        if colormap:
+            doc['binary'] = Binary(colormap_to_bytes(doc['colormap']))
+        else:
+            doc['binary'] = None
+
+        if public is not None and isinstance(public, bool):
+            self.setPublic(doc, public, save=False)
+
+        # give the current user admin access
+        self.setUserAccess(doc, user=creator, level=AccessType.ADMIN,
+                           save=False)
+
+        return self.save(doc)
+
+    def updateColormap(self, doc, updateUser=None):
+        """
+        Update a colormap.
+
+        :param docl: the colormap document to update.
+        :param updateUser: the user who is creating the update.
+        :returns: the colormap document that was updated.
+        """
+        doc['updated'] = datetime.datetime.utcnow()
+        doc['updatedId'] = updateUser['_id'] if updateUser else None
+        if 'colormap' in doc:
+            if doc['colormap']:
+                doc['binary'] = Binary(colormap_to_bytes(doc['colormap']))
+            else:
+                doc['binary'] = None
+        return self.save(doc)
+
+    def validate(self, doc):
+        return doc
--- server/rest.py
+++ server/rest.py
@@ -1,27 +1,17 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import json
 
-###############################################################################
-#  Girder plugin framework and tests adapted from Kitware Inc. source and
-#  documentation by the Imaging and Visualization Group, Advanced Biomedical
-#  Computational Science, Frederick National Laboratory for Cancer Research.
-#
-#  Copyright Kitware Inc.
-#
-#  Licensed under the Apache License, Version 2.0 ( the "License" );
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-###############################################################################
+from bson.objectid import ObjectId
 
-from girder.api.rest import Resource
+from girder import logger
+from girder.api import access
+from girder.api.describe import autoDescribeRoute, Description
+from girder.api.rest import Resource, filtermodel, setResponseHeader, setContentDisposition
+from girder.constants import AccessType, TokenScope
+from girder.exceptions import RestException
+from girder.models.model_base import ValidationException
+from girder.models.file import File
+from girder.utility import JsonEncoder
+from .models.colormap import Colormap
 
 
 class ColormapResource(Resource):
@@ -29,3 +19,189 @@ class ColormapResource(Resource):
         super(ColormapResource, self).__init__()
 
         self.resourceName = 'colormap'
+        self.route('GET', (), self.find)
+        self.route('POST', (), self.createColormap)
+        self.route('POST', ('file', ':fileId'), self.createColormapFromFile)
+        self.route('DELETE', (':id',), self.deleteColormap)
+        self.route('GET', (':id',), self.getColormap)
+        self.route('PUT', (':id',), self.updateColormap)
+        self.route('GET', (':id', 'access'), self.getColormapAccess)
+        self.route('PUT', (':id', 'access'), self.updateColormapAccess)
+        self.route('GET', (':id', 'download', ':name'), self.download)
+
+    @access.public(scope=TokenScope.DATA_READ)
+    @filtermodel(model='colormap', plugin='large_image')
+    @autoDescribeRoute(
+        Description('Search for colormaps.')
+        .responseClass('Colormap', array=True)
+        .param('name', 'The name of the colormap.', required=False)
+        .param('creatorId', 'The creator ID of the colormap.', required=False)
+        .pagingParams(defaultSort='name')
+        .errorResponse()
+        .errorResponse('No matching colormaps were found.', 404)
+    )
+    def find(self, name, creatorId, limit, offset, sort):
+        user = self.getCurrentUser()
+        query = {}
+        if name is not None:
+            query['name'] = name
+        if creatorId is not None:
+            query['creatorId'] = ObjectId(creatorId)
+        return list(Colormap().filterResultsByPermission(
+            cursor=Colormap().find(query, sort=sort),
+            user=user,
+            level=AccessType.READ,
+            limit=limit, offset=offset
+        ))
+
+    @access.user(scope=TokenScope.DATA_WRITE)
+    @filtermodel(model='colormap', plugin='large_image')
+    @autoDescribeRoute(
+        Description('Create a colormap.')
+        .responseClass('Colormap')
+        .param('name', 'Name for the colormap.', required=False, strip=True)
+        .param('public', 'Whether the colormap should be publicly visible.',
+               dataType='boolean', required=False, default=False)
+        .jsonParam('colormap', 'A JSON-encoded colormap.', requireObject=True)
+        .jsonParam('labels', 'A JSON-encoded labels.', required=False,
+                   requireObject=True)
+        .errorResponse('Write access was denied for the colormap.', 403)
+    )
+    def createColormap(self, name, public, colormap, labels):
+        try:
+            return Colormap().createColormap(
+                self.getCurrentUser(), colormap, name, labels, public)
+        except ValidationException as exc:
+            logger.exception('Failed to validate colormap')
+            raise RestException(
+                'Validation Error: JSON doesn\'t follow schema (%r).' % (
+                    exc.args, ))
+
+    @access.user(scope=TokenScope.DATA_WRITE)
+    @filtermodel(model='colormap', plugin='large_image')
+    @autoDescribeRoute(
+        Description('Create a colormap from a file.')
+        .responseClass('Colormap')
+        .param('name', 'Name for the colormap.', required=False, strip=True)
+        .param('public', 'Whether the colormap should be publicly visible.',
+               dataType='boolean', required=False)
+        .modelParam('fileId', model='file', level=AccessType.READ)
+        .errorResponse('Write access was denied for the colormap.', 403)
+    )
+    def createColormapFromFile(self, name, public, file):
+        params = json.load(File().open(file))
+        colormap = params.get('colormap', {})
+        if name is None:
+            name = params.get('name')
+        labels = params.get('labels', {})
+        if public is None:
+            public = params.get('public', False)
+        try:
+            return Colormap().createColormap(
+                self.getCurrentUser(), colormap, name, labels, public)
+        except ValidationException as exc:
+            logger.exception('Failed to validate colormap')
+            raise RestException(
+                'Validation Error: JSON doesn\'t follow schema (%r).' % (
+                    exc.args, ))
+
+    @access.user(scope=TokenScope.DATA_OWN)
+    @filtermodel(model='colormap', plugin='large_image')
+    @autoDescribeRoute(
+        Description('Delete a colormap.')
+        .modelParam('id', model='colormap', plugin='large_image',
+                    level=AccessType.WRITE)
+        .errorResponse('ID was invalid.')
+        .errorResponse('Write access was denied for the colormap.', 403)
+    )
+    def deleteColormap(self, colormap):
+        Colormap().remove(colormap)
+
+    @access.public(scope=TokenScope.DATA_READ)
+    @filtermodel(model='colormap', plugin='large_image')
+    @autoDescribeRoute(
+        Description('Get colormap by ID.')
+        .responseClass('Colormap')
+        .modelParam('id', model='colormap', plugin='large_image',
+                    level=AccessType.READ)
+        .errorResponse('ID was invalid.')
+        .errorResponse('Read access was denied for the colormap.', 403)
+    )
+    def getColormap(self, colormap):
+        return colormap
+
+    @access.user(scope=TokenScope.DATA_WRITE)
+    @filtermodel(model='colormap', plugin='large_image')
+    @autoDescribeRoute(
+        Description('Update a colormap.')
+        .responseClass('Colormap')
+        .modelParam('id', model='colormap', plugin='large_image',
+                    destName='model', level=AccessType.WRITE)
+        .param('name', 'Name for the colormap.', required=False)
+        .jsonParam('colormap', 'A JSON-encoded colormap.', required=False,
+                   requireObject=True)
+        .errorResponse('Write access was denied for the colormap.', 403)
+        .errorResponse('Color map not found.', 404)
+    )
+    def updateColormap(self, model, name, colormap):
+        user = self.getCurrentUser()
+        if name is not None:
+            model['name'] = name
+        if colormap is not None:
+            model['colormap'] = colormap
+        try:
+            return Colormap().updateColormap(model, updateUser=user)
+        except ValidationException as exc:
+            logger.exception('Failed to validate colormap')
+            raise RestException(
+                'Validation Error: JSON doesn\'t follow schema (%r).' % (
+                    exc.args, ))
+
+    @access.user(scope=TokenScope.DATA_OWN)
+    @filtermodel(model='colormap', plugin='large_image')
+    @autoDescribeRoute(
+        Description('Get the access control list for a colormap.')
+        .modelParam('id', model='colormap', plugin='large_image',
+                    level=AccessType.ADMIN)
+        .errorResponse('ID was invalid.')
+        .errorResponse('Admin access was denied for the colormap.', 403)
+    )
+    def getColormapAccess(self, colormap):
+        return Colormap().getFullAccessList(colormap)
+
+    @access.user(scope=TokenScope.DATA_OWN)
+    @filtermodel(model='colormap', plugin='large_image')
+    @autoDescribeRoute(
+        Description('Update the access control list for a colormap.')
+        .responseClass('Colormap')
+        .modelParam('id', model='colormap', plugin='large_image',
+                    level=AccessType.ADMIN)
+        .jsonParam('access', 'The JSON-encoded access control list.')
+        .param('public', 'Whether the colormap should be publicly visible.',
+               dataType='boolean', required=False)
+        .errorResponse('ID was invalid.')
+        .errorResponse('Admin access was denied for the colormap.', 403)
+    )
+    def updateColormapAccess(self, colormap, access, public):
+        Colormap().setPublic(colormap, public)
+        return Colormap().setAccessList(colormap, access, save=True,
+                                        user=self.getCurrentUser())
+
+    @access.public(scope=TokenScope.DATA_READ)
+    @autoDescribeRoute(
+        Description('Download colormap file.')
+        .modelParam('id', model=Colormap, level=AccessType.READ)
+        .param('name', 'The name of the file.', paramType='path')
+        .errorResponse('ID was invalid.')
+        .errorResponse('Read access was denied for the colormap.', 403)
+    )
+    def download(self, colormap, name):
+        setContentDisposition(name)
+
+        def stream():
+            yield json.dumps({'colormap': colormap['colormap']},
+                             sort_keys=True, allow_nan=False,
+                             cls=JsonEncoder).encode('utf8')
+
+        setResponseHeader('Content-Type', 'application/json')
+        return stream
